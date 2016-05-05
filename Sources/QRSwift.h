2f5ccdf2e15d8a7eb5f2595cae80669a1bd4f7bd
Chris Amanse <christopheramanse@gmail.com>
2016-05-05 12:03:07 +0800

    Add iOS and OSX framework targets

---
+++ Sources/QRSwift.h
@@ -0,0 +1,19 @@
+//
+//  QRSwift iOS.h
+//  QRSwift iOS
+//
+//  Created by Chris Amanse on 05/05/2016.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QRSwift iOS.
+FOUNDATION_EXPORT double QRSwiftVersionNumber;
+
+//! Project version string for QRSwift iOS.
+FOUNDATION_EXPORT const unsigned char QRSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRSwift_iOS/PublicHeader.h>
+
+
